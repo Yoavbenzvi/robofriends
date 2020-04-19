f9ca708b88609d33002198e1b259f3b7ad93149f
Yoav <yoav.benzvi@gmail.com>
2020-04-19 13:19:02 +0200

    'robots' state is now a part of the store added redux-logger and redux-thunk to deal with async actions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { searchRobots, requestRobots } from './reducers';
+import { logger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
-import App from './containers/App';
-import { searchRobots } from './reducers';
 import 'tachyons';
+import App from './containers/App';
 
+const rootReducer = combineReducers({
+	searchRobots,
+	requestRobots
+})
 
-const store = createStore(searchRobots)
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
 	<Provider store={store}>
