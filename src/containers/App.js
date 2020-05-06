bc03e6ea2d196992cfa4d92fd034575fb51ec78e
Yoav <yoav.benzvi@gmail.com>
2020-05-06 19:43:45 +0200

    rewrote App.js as a container component for testing purposes
    -App now contains a MainPage component that accepts all of App's props
    -testing is being done on MainPage instead of on app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,49 +1,25 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import Header from '../components/Header';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import MainPage from "../components/MainPage/MainPage";
 
-import { setSearchField, requestRobots } from '../actions';
+import { setSearchField, requestRobots } from "../actions";
 
-const mapStateToProps = (state) => ({                   
-   searchField: state.searchRobots.searchField,
-   robots: state.requestRobots.robots,
-   isPending: state.requestRobots.isPending,
-   error: state.requestRobots.error
-})
+const mapStateToProps = (state) => ({
+	searchField: state.searchRobots.searchField,
+	robots: state.requestRobots.robots,
+	isPending: state.requestRobots.isPending,
+	error: state.requestRobots.error,
+});
 
 const mapDispatchToProps = {
-   onSearchChange: (event) => setSearchField(event.target.value),
-   onRequestRobots: () => requestRobots()
-}
+	onSearchChange: (event) => setSearchField(event.target.value),
+	onRequestRobots: () => requestRobots(),
+};
 
 class App extends Component {
-
-   componentDidMount() {
-      this.props.onRequestRobots()
-   }
-
-   render() {
-      const { searchField, onSearchChange, robots, isPending } = this.props;
-      const filteredRobots = robots.filter(robot =>{
-         return robot.name.toLowerCase().includes(searchField.toLowerCase());
-      })
-
-      return isPending ?
-      <h1 className='tc'>Loading</h1> :
-      (
-         <div className='tc'>
-            <Header />
-            <SearchBox searchChange={onSearchChange}/>
-            <Scroll>
-               <CardList robots={filteredRobots} />
-            </Scroll>
-         </div>
-      );
-   }
+	render() {
+		return <MainPage {...this.props} />;
+	}
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
